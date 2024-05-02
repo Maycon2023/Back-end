6a914d889f6f3492edc89c8c42938eea61ec1a02
mayconedu <127303829+Maycon2023@users.noreply.github.com>
2024-05-02 17:43:02 -0300

    Add files via upload

---
+++ Back-end_maycon/exercicio-1/Calcule-idade.js
@@ -0,0 +1,7 @@
+const dataNascimento = new Date('12-04-2006');
+
+const anoReferencia = 2040;
+
+const idadeEm2040 = anoReferencia - dataNascimento.getFullYear();
+
+console.log("Sua idade em 2040 será : " + idadeEm2040 + "anos");
